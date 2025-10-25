89245aa586c5e8de3f5aecd695becf59e5631db5
yostena <yostenateferi12@gmail.com>
2025-10-25 15:10:17 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,8 @@
 USE alx_book_store;
+customer_id = 1
+customer_name = Cole Baidoo
+email = cbaidoo@sandtech.com
+address = 123 Happiness Ave.
 
 -- Insert a single customer into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
