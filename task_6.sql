1e7b8abf1d3b753a7ed236cf0fc06672d51836b7
yostena <yostenateferi12@gmail.com>
2025-10-25 16:45:11 +0300

    Fix SQL insert statement for Customers table

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,5 @@
-
-USE alx_book_store;
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Insert multiple customers into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
